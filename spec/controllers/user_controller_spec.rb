c22d63732bd1c46fc797204d1da7cc26219c1e22
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-05 22:31:42 -0800

    User controller redirect and render examples.

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -19,7 +19,12 @@ describe UserController do
         }.to change{User.count}.by(1)
       end
 
-      it 'redirects to the home page'
+      it 'redirects to the home page' do
+        post :create, user: FactoryGirl.attributes_for(:user)
+
+        response.should redirect_to root_url
+      end
+
     end
 
     context 'with invalid attributes' do
@@ -29,7 +34,12 @@ describe UserController do
         }.to_not change{User.count}
       end
 
-      it 're-renders the new template'
+      it 're-renders the new template' do
+        post :create, user: FactoryGirl.attributes_for(:invalid_user)
+
+        response.should render_template('new')
+      end
+
     end
 
   end
