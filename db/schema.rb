4ed93f0b95b8b8d72e838f35adb04f326f123ceb
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-05 21:21:44 -0800

    Add sessions database table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201215914) do
+ActiveRecord::Schema.define(:version => 20121206052011) do
 
   create_table "gend_images", :force => true do |t|
     t.string   "id_hash"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(:version => 20121201215914) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email"
