6f82dc0171fe54c1ce575ee0c4c559f48a115e49
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-15 00:32:36 -0800

    Move id_hash generation code to mixin.

---
+++ app/extras/id_hash_concern.rb
@@ -0,0 +1,21 @@
+# ActiveRecord::Model mixin to set a random id hash column.
+module IdHashConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :set_id_hash
+  end
+
+  def set_id_hash
+    self.id_hash = gen_id_hash
+  end
+
+  def gen_id_hash
+    begin
+      id_hash = SecureRandom.urlsafe_base64(4)
+    end while self.class.where(:id_hash => id_hash).exists?
+
+    id_hash
+  end
+
+end
--- app/models/src_image.rb
+++ app/models/src_image.rb
@@ -1,4 +1,6 @@
 class SrcImage < ActiveRecord::Base
+  include IdHashConcern
+
   attr_accessible :image, :url
 
   validates :content_type, :height, :image, :size, :width, presence: true
@@ -8,25 +10,11 @@ class SrcImage < ActiveRecord::Base
   belongs_to :user
   has_one :src_thumb
 
-  before_create :set_id_hash
-
   before_validation :set_derived_image_fields
 
   after_commit :create_thumbnail_job
 
   protected
-
-  def set_id_hash
-    self.id_hash = gen_id_hash
-  end
-
-  def gen_id_hash
-    begin
-      id_hash = SecureRandom.urlsafe_base64(4)
-    end while self.class.where(:id_hash => id_hash).exists?
-
-    id_hash
-  end
 
   def set_derived_image_fields
     if image
