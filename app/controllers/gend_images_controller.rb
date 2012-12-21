00582825c2de637e02fee66b366a1041af3d7f63
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-20 23:15:08 -0800

    Make gend_image id_hash act as id.
    Add gend_images controller spec.

--- app/controllers/gend_images_controller.rb
+++ app/controllers/gend_images_controller.rb
@@ -10,23 +10,24 @@ class GendImagesController < ApplicationController
   end
 
   def create
-    @gend_image = GendImage.new(params[:gend_image])
+    src_image = SrcImage.find_by_id_hash!(params[:gend_image][:src_image_id])
 
-    # TODO check if current user has access to the source image
+    @gend_image = GendImage.new
+    @gend_image.src_image = src_image
 
-    @gend_image.image = MemeCaptain.meme_top_bottom(
-      @gend_image.src_image.image, params[:text1], params[:text2],
+    @gend_image.image = MemeCaptain.meme_top_bottom(src_image.image,
+      params[:text1], params[:text2],
       :font => MemeCaptainWeb::Config::Font).to_blob
 
     if @gend_image.save
-      redirect_to :action => :show, :id => @gend_image.id
+      redirect_to :action => :show, :id => @gend_image.id_hash
     else
       render :new
     end
   end
 
   def show
-    gend_image = GendImage.find(params[:id])
+    gend_image = GendImage.find_by_id_hash!(params[:id])
 
     render :text => gend_image.image, :content_type => gend_image.content_type
   end
--- spec/controllers/gend_images_controller_spec.rb
+++ spec/controllers/gend_images_controller_spec.rb
@@ -10,14 +10,115 @@ describe GendImagesController do
   end
 
   describe "GET 'index'" do
-    it "returns http success" do
-      controller.stub_chain(:current_user, :gend_images) { [] }
 
-      get 'index'
+    before :each do
+      controller.stub_chain(:current_user, :gend_images) { [] }
+    end
+
+    subject { get 'index' }
+
+    it 'assigns the images' do
+      subject
+      expect(assigns(:gend_images)).to eq []
+    end
+
+    it "returns http success" do
+      subject
       expect(response).to be_success
     end
   end
 
-  it 'should have more specs'
+  describe "POST 'create'" do
+
+    let(:image) { File.read(Rails.root + 'spec/fixtures/files/ti_duck.jpg') }
+
+    let(:src_image) { mock_model(SrcImage, :image => image) }
+
+    context 'with valid attributes' do
+
+      before :each do
+        SrcImage.should_receive(:find_by_id_hash!).with('abc').and_return(
+          src_image)
+      end
+
+      it 'saves the new generated image to the database' do
+        expect {
+          post :create,
+            gend_image: { :src_image_id => 'abc' },
+            :text1 => 'hello',
+            :text2 => 'world'
+        }.to change{GendImage.count}.by(1)
+      end
+
+      it 'redirects to show the new image' do
+        post :create, gend_image: {
+          :src_image_id => 'abc',
+          :text1 => 'hello',
+          :text2 => 'world'
+        }
+
+        expect(response).to redirect_to :action => :show,
+          :id => assigns(:gend_image).id_hash
+      end
+
+    end
+
+    context 'when the source image is not found' do
+
+      it 'raises record not found' do
+        expect {
+          post :create, gend_image: { :src_image_id => 'abc' }
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+    end
+
+  end
+
+  describe "GET 'show'" do
+
+    context 'when the id is found' do
+
+      let(:gend_image) { mock_model(GendImage) }
+
+      before :each do
+        GendImage.should_receive(:find_by_id_hash!).and_return(gend_image)
+      end
+
+      it 'shows the source image' do
+        get 'show', :id => 'abc'
+
+        expect(response).to be_success
+      end
+
+      it 'has the right content type' do
+        gend_image.should_receive(:content_type).and_return('content type')
+
+        get 'show', :id => 'abc'
+
+        expect(response.content_type).to eq('content type')
+      end
+
+      it 'has the right content' do
+        gend_image.should_receive(:image).and_return('image')
+
+        get 'show', :id => 'abc'
+
+        expect(response.body).to eq('image')
+      end
+
+    end
+
+    context 'when the id is not found' do
+
+      it 'raises record not found' do
+        expect {
+          get 'show', :id => 'abc'
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+    end
+
+  end
 
 end
