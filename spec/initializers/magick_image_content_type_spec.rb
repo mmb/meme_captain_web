504fa00450112ea85a88a02d94a7659a7ee38247
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-22 15:49:45 -0800

    Add content_type png example.

--- spec/initializers/magick_image_content_type_spec.rb
+++ spec/initializers/magick_image_content_type_spec.rb
@@ -1,12 +1,24 @@
+require 'spec_helper'
+
 describe '#content_type' do
+
+  let(:jpeg) {
+    Magick::ImageList.new(Rails.root + 'spec/fixtures/files/ti_duck.jpg')[0]
+  }
+
+  let(:png) {
+    img_data = Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAcwAAAAyAgMAAACsWgPIAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wBHwQoJY1iyrwAAAAJUExURQAAAAAAAP///4Pdz9IAAAABdFJOUwBA5thmAAAAAWJLR0QB/wIt3gAAAHpJREFUWIXt1zESgCAMRNE03s9mG+9ns6e0EaKCqDOWf4sUJOHREvqciOn7Us0cEZiYmJhnc7HtVbJtS5LsVRo09DScZzmSG5iYmJit2RTVlduGquS920hFvxRMTEzMRzOv6TfKheMX9ThMTEzMnvlj5ld/S0xMTMxjNoGjc3pdi6L4AAAAAElFTkSuQmCC')
+    Magick::Image.from_blob(img_data)[0]
+  }
 
   it 'detects gif'
 
-  it 'detects jpeg'
+  it 'detects jpeg' do
+    expect(jpeg.content_type).to eq 'image/jpeg'
+  end
 
   it 'detects png' do
-    img_data = Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAcwAAAAyAgMAAACsWgPIAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wBHwQoJY1iyrwAAAAJUExURQAAAAAAAP///4Pdz9IAAAABdFJOUwBA5thmAAAAAWJLR0QB/wIt3gAAAHpJREFUWIXt1zESgCAMRNE03s9mG+9ns6e0EaKCqDOWf4sUJOHREvqciOn7Us0cEZiYmJhnc7HtVbJtS5LsVRo09DScZzmSG5iYmJit2RTVlduGquS920hFvxRMTEzMRzOv6TfKheMX9ThMTEzMnvlj5ld/S0xMTMxjNoGjc3pdi6L4AAAAAElFTkSuQmCC')
-    expect(Magick::Image.from_blob(img_data)[0].content_type).to eq('image/png')
+    expect(png.content_type).to eq 'image/png'
   end
 
 end
