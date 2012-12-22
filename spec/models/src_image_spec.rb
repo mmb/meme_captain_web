b993b09eb23af756c73937266975b383ba2f606c
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-22 15:22:15 -0800

    Use factories for image model specs.

--- spec/models/src_image_spec.rb
+++ spec/models/src_image_spec.rb
@@ -1,12 +1,6 @@
-require 'base64'
-
 require 'spec_helper'
 
 describe SrcImage do
-
-  let(:test_image) {
-    Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAcwAAAAyAgMAAACsWgPIAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wBHwQoJY1iyrwAAAAJUExURQAAAAAAAP///4Pdz9IAAAABdFJOUwBA5thmAAAAAWJLR0QB/wIt3gAAAHpJREFUWIXt1zESgCAMRNE03s9mG+9ns6e0EaKCqDOWf4sUJOHREvqciOn7Us0cEZiYmJhnc7HtVbJtS5LsVRo09DScZzmSG5iYmJit2RTVlduGquS920hFvxRMTEzMRzOv6TfKheMX9ThMTEzMnvlj5ld/S0xMTMxjNoGjc3pdi6L4AAAAAElFTkSuQmCC')
-  }
 
   it { should validate_presence_of :content_type }
   it { should_not allow_mass_assignment_of :content_type }
@@ -35,14 +29,14 @@ describe SrcImage do
 
   it 'should generate a unique id hash' do
     SrcImage.any_instance.stub(:gen_id_hash).and_return 'some_id_hash'
-    src_image = SrcImage.create(:image => test_image)
+    src_image = SrcImage.create(FactoryGirl.attributes_for(:src_image))
     expect(src_image.id_hash).to eq('some_id_hash')
   end
 
   context 'setting fields derived from the image' do
 
     subject {
-      src_image = SrcImage.new(:image => test_image)
+      src_image = SrcImage.new(FactoryGirl.attributes_for(:src_image))
       src_image.valid?
       src_image
     }
--- spec/models/src_thumb_spec.rb
+++ spec/models/src_thumb_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe SrcThumb do
 
-  let(:test_image) {
-    Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAcwAAAAyAgMAAACsWgPIAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wBHwQoJY1iyrwAAAAJUExURQAAAAAAAP///4Pdz9IAAAABdFJOUwBA5thmAAAAAWJLR0QB/wIt3gAAAHpJREFUWIXt1zESgCAMRNE03s9mG+9ns6e0EaKCqDOWf4sUJOHREvqciOn7Us0cEZiYmJhnc7HtVbJtS5LsVRo09DScZzmSG5iYmJit2RTVlduGquS920hFvxRMTEzMRzOv6TfKheMX9ThMTEzMnvlj5ld/S0xMTMxjNoGjc3pdi6L4AAAAAElFTkSuQmCC')
-  }
-
   it { should validate_presence_of :content_type }
   it { should allow_mass_assignment_of :content_type }
 
@@ -26,7 +22,7 @@ describe SrcThumb do
   context 'setting fields derived from the image' do
 
     subject {
-      src_thumb = SrcThumb.new(:image => test_image)
+      src_thumb = SrcThumb.new(FactoryGirl.attributes_for(:src_thumb))
       src_thumb.valid?
       src_thumb
     }
