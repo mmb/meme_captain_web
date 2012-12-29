efbb941eda62d7fc4667cbfd426b1f4e696bc74b
Matthew M. Boedicker <matthewm@boedicker.org>
2012-12-28 21:04:38 -0800

    Add partial views for src image and gend image.
    Use them in index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+
   def index
+    if current_user
+      @src_images = current_user.src_images.sort {
+          |a, b| b.updated_at <=> a.updated_at }
+
+      @gend_images = current_user.gend_images.sort {
+          |a, b| b.updated_at <=> a.updated_at }
+    else
+      @src_images = []
+      @gend_images = []
+    end
+
   end
+
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,6 +4,9 @@ describe HomeController do
 
   describe "GET 'index'" do
     it "returns http success" do
+      controller.stub_chain(:current_user, :src_images) { [] }
+      controller.stub_chain(:current_user, :gend_images) { [] }
+
       get 'index'
       expect(response).to be_success
     end
