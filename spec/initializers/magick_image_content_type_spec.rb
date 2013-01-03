2e1450c6372169bbe18404e108a7a96600404c82
Matthew M. Boedicker <matthewm@boedicker.org>
2013-01-02 23:22:17 -0800

    Gif content type spec.

--- spec/initializers/magick_image_content_type_spec.rb
+++ spec/initializers/magick_image_content_type_spec.rb
@@ -2,23 +2,18 @@ require 'spec_helper'
 
 describe '#content_type' do
 
-  let(:jpeg) {
-    Magick::ImageList.new(Rails.root + 'spec/fixtures/files/ti_duck.jpg')[0]
-  }
-
-  let(:png) {
-    img_data = Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAcwAAAAyAgMAAACsWgPIAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wBHwQoJY1iyrwAAAAJUExURQAAAAAAAP///4Pdz9IAAAABdFJOUwBA5thmAAAAAWJLR0QB/wIt3gAAAHpJREFUWIXt1zESgCAMRNE03s9mG+9ns6e0EaKCqDOWf4sUJOHREvqciOn7Us0cEZiYmJhnc7HtVbJtS5LsVRo09DScZzmSG5iYmJit2RTVlduGquS920hFvxRMTEzMRzOv6TfKheMX9ThMTEzMnvlj5ld/S0xMTMxjNoGjc3pdi6L4AAAAAElFTkSuQmCC')
-    Magick::Image.from_blob(img_data)[0]
-  }
-
-  it 'detects gif'
-
-  it 'detects jpeg' do
-    expect(jpeg.content_type).to eq 'image/jpeg'
+  def image_fixture(path)
+    Magick::ImageList.new(Rails.root + "spec/fixtures/files/#{path}")[0]
   end
 
-  it 'detects png' do
-    expect(png.content_type).to eq 'image/png'
-  end
+  let(:gif) { image_fixture('ti_duck.gif') }
+  let(:jpeg) { image_fixture('ti_duck.jpg') }
+  let(:png) { image_fixture('ti_duck.png') }
+
+  it('detects gif') { expect(gif.content_type).to eq 'image/gif' }
+
+  it('detects jpeg') { expect(jpeg.content_type).to eq 'image/jpeg' }
+
+  it('detects png') { expect(png.content_type).to eq 'image/png' }
 
 end
