e0e7d2ca09de2666c9802006230716783ae20ab2
Matthew M. Boedicker <matthewm@boedicker.org>
2013-01-28 22:54:43 -0800

    Don't error on a source set with no user.

--- app/models/src_set.rb
+++ app/models/src_set.rb
@@ -40,6 +40,10 @@ class SrcSet < ActiveRecord::Base
     thumb.height if thumb
   end
 
+  def by
+    try(:user).try(:email)
+  end
+
   scope :active, where(:is_deleted => false)
 
   scope :owned_by, lambda { |user| where(:user_id => user.id) }
