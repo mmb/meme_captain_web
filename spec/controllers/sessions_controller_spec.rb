08069983ac811aeeda9065d099d2aed0d81b8021
Matthew M. Boedicker <matthewm@boedicker.org>
2013-02-09 12:09:35 -0800

    Refactor using subject.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,36 +3,38 @@ require 'spec_helper'
 describe SessionsController do
 
   let(:mock_user) { mock_model(User) }
-  let(:user) { User.create!(FactoryGirl.attributes_for(:user)) }
+  let(:user) { FactoryGirl.create(:user) }
 
   describe "POST 'create'" do
+
+    subject { post :create, :email => user.email, :password => user.password }
 
     context 'when login succeeds' do
 
       it 'authenticates the user' do
         User.should_receive(:find_by_email).with(user.email).
-          and_return(mock_user)
+            and_return(mock_user)
 
         mock_user.should_receive(:authenticate).with(user.password).
-          and_return(true)
+            and_return(true)
 
-        post :create, :email => user.email, :password => user.password
+        subject
       end
 
       it 'creates a session' do
-        post :create, :email => user.email, :password => user.password
+        subject
 
         expect(session[:user_id]).to eq(user.id)
       end
 
       it 'redirects to the root url' do
-        post :create, :email => user.email, :password => user.password
+        subject
 
         expect(response).to redirect_to root_url
       end
 
       it 'informs the user of login success with flash' do
-        post :create, :email => user.email, :password => user.password
+        subject
 
         expect(flash[:notice]).to eq('Logged in.')
       end
@@ -40,19 +42,20 @@ describe SessionsController do
 
     context 'when login fails' do
 
-      before(:each) do
-        post :create, :email => user.email, :password => 'wrongpass'
-      end
+      subject { post :create, :email => user.email, :password => 'wrongpass' }
 
       it 'does not create a session' do
+        subject
         expect(session[:user_id]).to be_nil
       end
 
       it 'renders the new template' do
+        subject
         expect(response).to render_template('new')
       end
 
       it 'informs the user of login failure with flash' do
+        subject
         expect(flash[:error]).to eq('Login failed.')
       end
 
@@ -62,25 +65,26 @@ describe SessionsController do
 
   describe "DELETE 'destroy'" do
 
+    subject { delete :destroy }
+
     context 'when the user is logged in ' do
 
       before do
         post :create, :email => user.email, :password => user.password
       end
 
-      before(:each) do
-        delete :destroy
-      end
-
       it 'clears the session' do
+        subject
         expect(session[:user_id]).to be_nil
       end
 
       it 'redirects to the root url' do
+        subject
         expect(response).to redirect_to root_url
       end
 
       it 'informs the user of login failure with flash' do
+        subject
         expect(flash[:notice]).to eq('Logged out.')
       end
 
@@ -88,15 +92,13 @@ describe SessionsController do
 
     context 'when the user is logged out' do
 
-      before(:each) do
-        delete :destroy
-      end
-
       it 'clears the session' do
+        subject
         expect(session[:user_id]).to be_nil
       end
 
       it 'redirects to the root url' do
+        subject
         expect(response).to redirect_to root_url
       end
 
