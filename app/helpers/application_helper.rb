3707b0badf7de9d80c1b87faf59ee5c3078acb59
Matthew M. Boedicker <matthewm@boedicker.org>
2013-02-18 22:20:58 -0800

    Handle user id from session not being found.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+
+  let(:user) { user = FactoryGirl.create(:user) }
+
+  context 'when the user id is found' do
+    it 'sets current user to the correct user' do
+      session[:user_id] = user.id
+      expect(current_user).to eq user
+    end
+  end
+
+  context 'when the user id not found' do
+    it 'sets current user to nil' do
+      session[:user_id] = 'not found'
+      expect(current_user).to be_nil
+    end
+  end
 end
