5e9c4263f04c62a7c3b6b5911049956667a7a96d
Matthew M. Boedicker <matthewm@boedicker.org>
2013-02-20 22:09:33 -0800

    Show more images to users that aren't logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    @src_sets = SrcSet.active.most_recent(6)
-
     if current_user
+      @src_sets = SrcSet.active.most_recent(8)
       @src_images = SrcImage.without_image.includes(:src_thumb).owned_by(current_user).active.most_recent(6)
       @gend_images = GendImage.without_image.includes(:gend_thumb).owned_by(current_user).active.most_recent(6)
+    else
+      @src_sets = SrcSet.active.most_recent(16)
     end
 
   end
-
 end
