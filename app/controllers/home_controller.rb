95971ce4f90700f4184e46c8e851786debaafae5
Matthew M. Boedicker <matthewm@boedicker.org>
2013-03-04 22:29:50 -0800

    Optimize row counts for most common browser widths.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
 
+  PER_ROW = 7
+
   def index
     if current_user
-      @src_sets = SrcSet.active.most_recent(8)
-      @src_images = SrcImage.without_image.includes(:src_thumb).owned_by(current_user).active.most_recent(6)
-      @gend_images = GendImage.without_image.includes(:gend_thumb).owned_by(current_user).active.most_recent(6)
+      @src_sets = SrcSet.active.most_recent(PER_ROW)
+      @src_images = SrcImage.without_image.includes(:src_thumb).owned_by(current_user).active.most_recent(PER_ROW)
+      @gend_images = GendImage.without_image.includes(:gend_thumb).owned_by(current_user).active.most_recent(2 * PER_ROW)
     else
-      @src_sets = SrcSet.active.most_recent(16)
-      @gend_images = GendImage.without_image.includes(:gend_thumb).owned_by(current_user).active.most_recent(16)
+      @src_sets = SrcSet.active.most_recent(2 * PER_ROW)
+      @gend_images = GendImage.without_image.includes(:gend_thumb).owned_by(current_user).active.most_recent(3 * PER_ROW)
     end
-
   end
+
 end
