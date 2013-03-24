2b96a759b283a063eaba6630dde82384af563e4e
Matthew M. Boedicker <matthewm@boedicker.org>
2013-03-23 23:32:07 -0700

    Only show src sets with quality >= 50 on front page.

--- app/models/src_set.rb
+++ app/models/src_set.rb
@@ -64,4 +64,6 @@ class SrcSet < ActiveRecord::Base
   scope :owned_by, lambda { |user| where(:user_id => user.id) }
 
   scope :most_recent, lambda { |limit=1| order(:quality, :updated_at).reverse_order.limit(limit) }
+
+  scope :front_page, where('quality >= 50')
 end
