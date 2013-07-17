8e528dfde9a36686d2a195848086a8535d0b76fe
Matthew M. Boedicker <matthewm@boedicker.org>
2013-07-16 23:36:30 -0700

    Hide gend image checkboxes if toolbar is disabled.

--- spec/views/gend_images/_gend_image.html.erb_spec.rb
+++ spec/views/gend_images/_gend_image.html.erb_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe 'gend_images/_gend_image.html' do
+  let(:gend_thumb) { mock_model(GendThumb, width: 19, height: 80) }
+
+  let(:gend_image) { mock_model(
+      GendImage,
+      work_in_progress: false,
+      gend_thumb: gend_thumb,
+      id_hash: 'id_hash') }
+
+  let(:show_toolbar) { true }
+
+  before do
+    assign :show_toolbar, show_toolbar
+  end
 
   subject {
     render :partial => 'gend_images/gend_image',
-           :locals => {:gend_image => gend_image}
+           :locals => { gend_image: gend_image }
   }
 
   context 'the image has been processed' do
-    let(:gend_thumb) { mock_model(GendThumb, :width => 19, :height => 80) }
-    let(:gend_image) { mock_model(GendImage,
-                                  :work_in_progress => false,
-                                  :gend_thumb => gend_thumb,
-                                  :id_hash => 'id_hash') }
 
     it 'shows the thumbnail' do
       subject
@@ -45,6 +53,24 @@ describe 'gend_images/_gend_image.html' do
       expect(rendered).to match('Under Construction')
     end
 
+  end
+
+  context 'when the toolbar is enabled' do
+    it 'shows the checkbox' do
+      subject
+
+      expect(rendered).to have_selector '.selector'
+    end
+  end
+
+  context 'when the toolbar is disabled' do
+    let(:show_toolbar) { false }
+
+    it 'hides the checkbox' do
+      subject
+
+      expect(rendered).to_not have_selector '.selector'
+    end
   end
 
 end
