db5de8630a169161dfa8329553ab088e881bf233
Matthew M. Boedicker <matthewm@boedicker.org>
2013-08-28 21:44:17 -0700

    Convert to ruby 1.9 style hashes.
    Add encoding.

--- spec/views/gend_images/new.html.erb_spec.rb
+++ spec/views/gend_images/new.html.erb_spec.rb
@@ -1,9 +1,11 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe 'gend_images/new.html.erb' do
 
-  let(:src_image) { stub_model(SrcImage, :id_hash => 'abc') }
-  let(:gend_image) { stub_model(GendImage, :src_image => src_image) }
+  let(:src_image) { stub_model(SrcImage, id_hash: 'abc') }
+  let(:gend_image) { stub_model(GendImage, src_image: src_image) }
 
   it 'renders' do
     assign(:gend_image, gend_image)
