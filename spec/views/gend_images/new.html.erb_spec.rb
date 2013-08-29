9cc271c752c5fb669028183744d30c1d1eb1026e
Matthew M. Boedicker <matthewm@boedicker.org>
2013-08-28 22:20:46 -0700

    Use src image name as title on new meme page.

--- spec/views/gend_images/new.html.erb_spec.rb
+++ spec/views/gend_images/new.html.erb_spec.rb
@@ -4,12 +4,21 @@ require 'spec_helper'
 
 describe 'gend_images/new.html.erb' do
 
-  let(:src_image) { stub_model(SrcImage, id_hash: 'abc') }
+  let(:src_image) { stub_model(SrcImage, id_hash: 'abc', name: 'src image name') }
   let(:gend_image) { stub_model(GendImage, src_image: src_image) }
 
-  it 'renders' do
+  before do
     assign(:gend_image, gend_image)
+  end
+
+  it 'renders' do
     render
+  end
+
+  it 'uses the src image name as an h1' do
+    render
+
+    expect(rendered).to have_selector('h1', content: 'src image name')
   end
 
 end
