fef60e1d96942c114814833c38b40f247a24571e
Matthew M. Boedicker <matthewm@boedicker.org>
2013-09-02 00:22:11 -0700

    Make page title settable.
    Fully specify google analytics partial.

---
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe 'layouts/application.html.erb' do
+
+  it 'has the right title' do
+    render
+
+    expect(rendered).to have_selector('title', content: 'Meme Captain meme generator')
+  end
+
+end
