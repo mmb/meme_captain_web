091e287fcf3e5465d70f6d1aa383b123e33a8e98
Matthew M. Boedicker <matthewm@boedicker.org>
2013-09-03 23:10:25 -0700

    Use src set name in src set show title.

---
+++ spec/views/src_sets/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe 'src_sets/show.html.erb' do
+
+  let(:src_set) { stub_model(SrcSet, name: 'src set') }
+
+  before do
+    assign(:src_set, src_set)
+    assign(:src_images, Kaminari.paginate_array([]).page(1))
+  end
+
+  it 'has the src set name in the title' do
+    render
+
+    expect(view.content_for(:title)).to eq 'src set memes'
+  end
+
+end
