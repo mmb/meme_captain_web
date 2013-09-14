5df857c60b57dae90f2388dd4d9bb738a7eb5a43
Matthew M. Boedicker <matthewm@boedicker.org>
2013-09-14 14:07:41 -0700

    Rubocop style recommendations.

--- spec/views/gend_images/new.html.erb_spec.rb
+++ spec/views/gend_images/new.html.erb_spec.rb
@@ -4,9 +4,13 @@ require 'spec_helper'
 
 describe 'gend_images/new.html.erb' do
 
-  let(:src_image) { stub_model(SrcImage, id_hash: 'abc', name: 'src image name') }
+  let(:src_image) do
+    stub_model(SrcImage, id_hash: 'abc', name: 'src image name')
+  end
   let(:caption1) { stub_model(Caption, text: 'text 1') }
-  let(:gend_image) { stub_model(GendImage, src_image: src_image, captions: [caption1]) }
+  let(:gend_image) do
+    stub_model(GendImage, src_image: src_image, captions: [caption1])
+  end
 
   before do
     assign(:gend_image, gend_image)
@@ -32,7 +36,8 @@ describe 'gend_images/new.html.erb' do
   it 'has xxlarge text inputs' do
     render
 
-    expect(rendered).to have_selector('#gend_image_captions_attributes_0_text') do |s|
+    expect(rendered).to have_selector(
+                            '#gend_image_captions_attributes_0_text') do |s|
       expect(s.first['class']).to_not be_nil
       expect(s.first['class']).to include 'input-xxlarge'
     end
