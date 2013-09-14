6b409806a613700e34abac50d2db6d6100b80ed3
Matthew M. Boedicker <matthewm@boedicker.org>
2013-09-14 14:05:09 -0700

    Make caption inputs bigger.

--- spec/views/gend_images/new.html.erb_spec.rb
+++ spec/views/gend_images/new.html.erb_spec.rb
@@ -5,10 +5,12 @@ require 'spec_helper'
 describe 'gend_images/new.html.erb' do
 
   let(:src_image) { stub_model(SrcImage, id_hash: 'abc', name: 'src image name') }
-  let(:gend_image) { stub_model(GendImage, src_image: src_image) }
+  let(:caption1) { stub_model(Caption, text: 'text 1') }
+  let(:gend_image) { stub_model(GendImage, src_image: src_image, captions: [caption1]) }
 
   before do
     assign(:gend_image, gend_image)
+    assign(:caption_defaults, [{ autofocus: true }, {}])
   end
 
   it 'renders' do
@@ -25,6 +27,16 @@ describe 'gend_images/new.html.erb' do
     render
 
     expect(view.content_for(:title)).to eq 'src image name meme'
+  end
+
+  it 'has xxlarge text inputs' do
+    render
+
+    expect(rendered).to have_selector('#gend_image_captions_attributes_0_text') do |s|
+      expect(s.first['class']).to_not be_nil
+      expect(s.first['class']).to include 'input-xxlarge'
+    end
+
   end
 
 end
