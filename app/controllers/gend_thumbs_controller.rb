462ba0e55ef86670c8eb0a7494102d8173f5a3ee
Matthew M. Boedicker <matthewm@boedicker.org>
2013-09-19 21:39:07 -0700

    Increase cache max age for gend thumbs to one week.

--- app/controllers/gend_thumbs_controller.rb
+++ app/controllers/gend_thumbs_controller.rb
@@ -5,7 +5,7 @@ class GendThumbsController < ApplicationController
   def show
     gend_thumb = GendThumb.find(params[:id], select: :image)
 
-    expires_in 1.hour, public: true
+    expires_in 1.week, public: true
 
     if stale?(gend_thumb)
       render text: gend_thumb.image, content_type: gend_thumb.content_type
--- spec/controllers/gend_thumbs_controller_spec.rb
+++ spec/controllers/gend_thumbs_controller_spec.rb
@@ -36,6 +36,14 @@ describe GendThumbsController do
         expect(response.body).to eq('image')
       end
 
+      it 'has the correct Expires header' do
+        GendThumb.should_receive(:find).and_return(gend_thumb)
+
+        get :show, id: 1
+
+        expect(response.headers['Cache-Control']).to eq 'max-age=604800, public'
+      end
+
     end
 
     context 'when the id is not found' do
