a2d54d7f4fa15be873f1cbb97d55bbcb7c2db3b1
Matthew M. Boedicker <matthewm@boedicker.org>
2014-01-04 23:49:41 -0800

    Rubocop style recommendations.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,16 +1,18 @@
+# encoding: UTF-8
+
 module MemeCaptainWeb
-
+  # Configuration constants.
   module Config
-
     # Maximum size of any side for generated thumbnail images.
     ThumbSide = 128
 
-    # Source images with any side longer than MaxSourceImageSide will be reduced. After reduction their maximum side
-    # will be MaxSourceImageSide.
+    # Source images with any side longer than MaxSourceImageSide will be
+    # reduced. After reduction their maximum side will be MaxSourceImageSide.
     MaxSourceImageSide = 800
 
-    # Source images with their longest side shorter than MinSourceImageSide will be enlarged. After enlargement their
-    # maximum side will be EnlargedSourceImageSide.
+    # Source images with their longest side shorter than MinSourceImageSide
+    # will be enlarged. After enlargement their maximum side will be
+    # EnlargedSourceImageSide.
     MinSourceImageSide = 400
 
     EnlargedSourceImageSide = 600
@@ -18,7 +20,7 @@ module MemeCaptainWeb
     # Minimum source set quality to be shown on the front page.
     SetFrontPageMinQuality = (ENV['MC_SET_FRONT_PAGE_MIN_QUALITY'] || 0).to_i
 
-    GendImageHost = ENV['GEND_IMAGE_HOST'].blank? ? nil : ENV['GEND_IMAGE_HOST']
+    GendImageHost =
+        ENV['GEND_IMAGE_HOST'].blank? ? nil : ENV['GEND_IMAGE_HOST']
   end
-
 end
