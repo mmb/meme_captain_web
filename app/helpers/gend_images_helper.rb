d8289e59cfb4fde8c566423b6235fbe1f0114677
Matthew M. Boedicker <matthewm@boedicker.org>
2014-02-23 21:31:30 -0800

    Rubocop style recommendations.

--- app/helpers/gend_images_helper.rb
+++ app/helpers/gend_images_helper.rb
@@ -1,5 +1,7 @@
-module GendImagesHelper
+# encoding: UTF-8
 
+# Build urls for generated images.
+module GendImagesHelper
   def gend_image_url_for(gend_image)
     url_for(
         controller: :gend_images,
@@ -9,5 +11,4 @@ module GendImagesHelper
         host: MemeCaptainWeb::Config::GendImageHost || request.host
     )
   end
-
 end
