84cef079adee9120f26168c19fc2b2661fc5343c
Matthew M. Boedicker <matthewm@boedicker.org>
2014-04-16 22:29:58 -0700

    Build initial captions with correct attributes.
    Instead of using @caption_defaults.

--- app/controllers/gend_images_controller.rb
+++ app/controllers/gend_images_controller.rb
@@ -7,16 +7,16 @@ class GendImagesController < ApplicationController
     @src_image_path = url_for(controller: :src_images, action: :show,
                               id: @gend_image.src_image.id_hash)
 
-    @caption_defaults = [
-        {
-            top_left_y_pct: 0
-        },
-        {
-            top_left_y_pct: 0.75
-        }
-    ]
-
-    2.times { @gend_image.captions.build }
+    @gend_image.captions.build(
+        top_left_x_pct: 0.05,
+        top_left_y_pct: 0,
+        width_pct: 0.9,
+        height_pct: 0.25)
+    @gend_image.captions.build(
+        top_left_x_pct: 0.05,
+        top_left_y_pct: 0.75,
+        width_pct: 0.9,
+        height_pct: 0.25)
   end
 
   def index
