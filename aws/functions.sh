e5d051b8076b8cc87fa48bec75b31b7d34b7ebc1
Matthew M. Boedicker <matthewm@boedicker.org>
2015-04-25 21:53:34 -0700

    Add sleep in all instances healthy check.

--- aws/functions.sh
+++ aws/functions.sh
@@ -3,6 +3,7 @@ wait_for_all_healthy() {
     ALL_HEALTHY=`aws elb describe-instance-health --load-balancer-name memecaptain | jq '.InstanceStates | map(.State=="InService") | all'`
     echo `date` all instances healthy? $ALL_HEALTHY
     if [ "$ALL_HEALTHY" = "true" ]; then break; fi
+    sleep 4
   done
 }
 
