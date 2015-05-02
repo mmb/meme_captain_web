0da1f4c8096095e42c721e5f7d28447e6862883b
Matthew M. Boedicker <matthewm@boedicker.org>
2015-05-01 23:03:08 -0700

    Add test for SetFrontPageMinQuality config.

--- spec/initializers/config_spec.rb
+++ spec/initializers/config_spec.rb
@@ -4,18 +4,34 @@ require 'rails_helper'
 
 describe MemeCaptainWeb::Config do
 
-  context 'when the MC_SET_FRONT_PAGE_MIN_QUALITY env var is not set' do
+  let(:rb_path) do
+    File.expand_path('../../../config/initializers/config.rb', __FILE__)
+  end
 
+  after(:all) do
+    # Avoid polluting config module for later tests.
+    rb_path = File.expand_path(
+      '../../../config/initializers/config.rb', __FILE__)
+    Kernel.silence_warnings { load(rb_path) }
+  end
+
+  context 'when the MC_SET_FRONT_PAGE_MIN_QUALITY env var is not set' do
     it 'uses the default' do
+      Kernel.silence_warnings { load(rb_path) }
       expect(MemeCaptainWeb::Config::SetFrontPageMinQuality).to eq 0
     end
-
   end
 
   context 'when the MC_SET_FRONT_PAGE_MIN_QUALITY env var is set' do
+    before do
+      stub_const(
+        'ENV',
+        ENV.to_hash.merge('MC_SET_FRONT_PAGE_MIN_QUALITY' => '50'))
+    end
 
-    xit 'uses the environment variable'
-
+    it 'uses the environment variable' do
+      Kernel.silence_warnings { load(rb_path) }
+      expect(MemeCaptainWeb::Config::SetFrontPageMinQuality).to eq(50)
+    end
   end
-
 end
