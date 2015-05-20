7efaef9029423f2b45d4f6a06ed9805194d13980
Matthew M. Boedicker <matthewm@boedicker.org>
2015-05-19 22:01:47 -0700

    Make migration work on MySQL.
    Editing an old migration is bad but this migration blows up on
    MySQL so no new migrations that could fix it get a chance to run.

--- db/migrate/20130627041103_add_description_to_src_images.rb
+++ db/migrate/20130627041103_add_description_to_src_images.rb
@@ -2,7 +2,7 @@ class AddDescriptionToSrcImages < ActiveRecord::Migration
 
   def change
     add_column :src_images, :name, :text
-    add_index :src_images, :name
+    add_index :src_images, :name, length: 64
   end
 
 end
