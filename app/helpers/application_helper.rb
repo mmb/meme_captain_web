9656d4ba458116e701366fb101dea485790d89fa
Matthew M. Boedicker <matthewm@boedicker.org>
2015-05-22 22:32:31 -0700

    Use find_by instead of dynamic finder.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@
 # Application helper.
 module ApplicationHelper
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def cache_expires(length_of_time)
