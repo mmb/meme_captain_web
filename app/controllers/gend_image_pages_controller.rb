b612be189cb7816b1701de606a0c0ca7ae949c61
Matthew M. Boedicker <matthewm@boedicker.org>
2015-05-22 22:37:28 -0700

    Use find_by instead of dynamic finder.

--- app/controllers/gend_image_pages_controller.rb
+++ app/controllers/gend_image_pages_controller.rb
@@ -5,8 +5,8 @@ class GendImagePagesController < ApplicationController
   include GendImagesHelper
 
   def show
-    @gend_image = GendImage.without_image.active.find_by_id_hash!(
-      params[:id])
+    @gend_image = GendImage.without_image.active.find_by!(
+      id_hash: params[:id])
 
     @src_image = SrcImage.without_image.find(@gend_image.src_image_id)
     @gend_image_url = gend_image_url_for(@gend_image)
