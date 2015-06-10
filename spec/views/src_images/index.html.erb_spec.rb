4ef8590c5d5f99bd451f0facb1afa15ee9d9da28
Matthew M. Boedicker <matthewm@boedicker.org>
2015-06-09 22:00:47 -0700

    Add spec for src images index view.
    Remove unnecessary if in src images index view.

--- spec/views/src_images/index.html.erb_spec.rb
+++ spec/views/src_images/index.html.erb_spec.rb
@@ -1,5 +1,12 @@
 # encoding: UTF-8
 
-describe 'src_images/index.html.erb' do
-  pending "add some examples to (or delete) #{__FILE__}"
+require 'rails_helper'
+
+describe 'src_images/index.html.erb', type: :view do
+  it 'renders the src images partial' do
+    assign(:src_images, Kaminari.paginate_array([]).page(1))
+    allow(view).to receive(:render).and_call_original
+    expect(view).to receive(:render).with(partial: 'src_images/src_images')
+    render
+  end
 end
