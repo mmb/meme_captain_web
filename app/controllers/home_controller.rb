5275178b1d7d01b9d210abd549b917b848e97993
Matthew M. Boedicker <matthewm@boedicker.org>
2015-07-19 01:31:34 -0700

    Refactor home and my controller index methods.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,25 @@
 # Home page controller.
 class HomeController < ApplicationController
   def index
-    @src_images = SrcImage.without_image.includes(
-      :src_thumb).publick.active.finished.most_used(12)
-    @src_sets = SrcSet.front_page.active.not_empty.most_recent(12)
-    @gend_images = GendImage.without_image.includes(
-      :gend_thumb).publick.active.finished.most_recent(24)
-
+    @src_images = src_images
+    @src_sets = src_sets
+    @gend_images = gend_images
     @show_toolbar = false
+  end
+
+  private
+
+  def src_images
+    SrcImage.without_image.includes(
+      :src_thumb).publick.active.finished.most_used(12)
+  end
+
+  def src_sets
+    SrcSet.front_page.active.not_empty.most_recent(12)
+  end
+
+  def gend_images
+    GendImage.without_image.includes(
+      :gend_thumb).publick.active.finished.most_recent(24)
   end
 end
--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -8,13 +8,25 @@ class MyController < ApplicationController
     @name = current_user.email
     @avatar_url = current_user.avatar.image_url
 
-    @src_sets = SrcSet.owned_by(current_user).active.most_recent.page(
-      params[:page])
-    @src_images = SrcImage.without_image.includes(:src_thumb).owned_by(
-      current_user).active.most_recent.page(params[:page])
-    @gend_images = GendImage.without_image.includes(:gend_thumb).owned_by(
-      current_user).active.most_recent.page(params[:page])
-
+    @src_sets = src_sets
+    @src_images = src_images
+    @gend_images = gend_images
     @show_toolbar = true
+  end
+
+  private
+
+  def src_sets
+    SrcSet.owned_by(current_user).active.most_recent.page(params[:page])
+  end
+
+  def src_images
+    SrcImage.without_image.includes(:src_thumb).owned_by(
+      current_user).active.most_recent.page(params[:page])
+  end
+
+  def gend_images
+    GendImage.without_image.includes(:gend_thumb).owned_by(
+      current_user).active.most_recent.page(params[:page])
   end
 end
