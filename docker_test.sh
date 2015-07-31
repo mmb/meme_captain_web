0da3f6d341ed013aed893096cce13b92aae97985
Matthew M. Boedicker <matthewm@boedicker.org>
2015-07-30 22:39:32 -0700

    Script for creating and testing docker image.

---
+++ docker_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+docker build -t meme_captain_test .
+
+docker run \
+  -d \
+  -e RAILS_ENV=production \
+  -e SECRET_KEY_BASE=secret \
+  --name=meme_captain_test \
+  -p 6081:6081 \
+  --privileged=true \
+  meme_captain_test
+
+docker exec meme_captain_test rake db:migrate
+
+IP=$(echo $DOCKER_HOST | tr -c -d [:digit:].: | cut -d : -f 2)
+
+curl $IP:6081
+
+# cucumber
+
+docker stop meme_captain_test
