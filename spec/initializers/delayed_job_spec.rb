bd62edb60150cb7d4dce34e18533b4401aa6f93c
Matthew M. Boedicker <matthewm@boedicker.org>
2015-07-30 20:36:57 -0700

    Revert "Revert "Do not destroy failed delayed jobs.""
    This reverts commit 38f3f69d6b3849e04f91163075a6aee2362ec9b3.

--- spec/initializers/delayed_job_spec.rb
+++ spec/initializers/delayed_job_spec.rb
@@ -4,4 +4,8 @@ describe 'delayed job initializer' do
   it 'sets max_attempts to 4' do
     expect(Delayed::Worker.max_attempts).to eq(4)
   end
+
+  it 'sets destroy_failed_jobs to false' do
+    expect(Delayed::Worker.destroy_failed_jobs).to be(false)
+  end
 end
