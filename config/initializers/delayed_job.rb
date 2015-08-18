066ad8625b2f7fbcf88cf8eddb7594567091c30f
Matthew M. Boedicker <matthewm@boedicker.org>
2015-08-17 22:39:22 -0700

    Delayed job logs to separate log file.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,3 +1,4 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 4
 Delayed::Worker.max_run_time = 90.seconds
+Delayed::Worker.logger = Logger.new(Rails.root.join('log', 'delayed_job.log'))
