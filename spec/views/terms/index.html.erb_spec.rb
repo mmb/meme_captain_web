697b774f4ae827f1714bd3fcf8960f28da8dcf81
Matthew M. Boedicker <matthewm@boedicker.org>
2015-08-23 23:51:38 -0700

    Set title on terms of service.

---
+++ spec/views/terms/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+require 'rails_helper'
+
+describe 'terms/index.html.erb', type: :view do
+  include Webrat::Matchers
+
+  it 'sets the content for the title' do
+    expect(view).to receive(:content_for).with(:title) do |&block|
+      expect(block.call).to eq('Meme Captain Terms of Service')
+    end
+    render
+  end
+end
