2d1f1094fd1e714fb6e6a95cff86f005351f49ca
Matthew M. Boedicker <matthewm@boedicker.org>
2015-08-25 00:07:47 -0700

    Script to cleanup old AMIs.

---
+++ aws/ami_cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+aws \
+  ec2 \
+  describe-images \
+  --owners self \
+  --filters 'Name=name,Values=memecaptain-*' \
+  | \
+  jq \
+  --raw-output \
+  '.Images | sort_by(.CreationDate) | reverse | .[3:-1][] | .ImageId+" "+.BlockDeviceMappings[0].Ebs.SnapshotId' \
+  | \
+  while read AMI_ID SNAPSHOT_ID; do
+    aws ec2 deregister-image --image-id $AMI_ID
+    aws ec2 delete-snapshot --snapshot-id $SNAPSHOT_ID
+  done
