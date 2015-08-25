81de992c1b574049b4c8ffb28d987d248709870c
Matthew M. Boedicker <matthewm@boedicker.org>
2015-08-24 22:04:44 -0700

    Remove default max_attempts from delayed job config.
    Each job defines its own max_attempts.

--- spec/initializers/delayed_job_spec.rb
+++ spec/initializers/delayed_job_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe 'delayed job initializer' do
-  it 'sets max_attempts to 4' do
-    expect(Delayed::Worker.max_attempts).to eq(3)
-  end
-
   it 'sets destroy_failed_jobs to false' do
     expect(Delayed::Worker.destroy_failed_jobs).to be(false)
   end
