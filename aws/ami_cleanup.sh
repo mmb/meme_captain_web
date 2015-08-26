126242026a7c6a7c53321b9e834d71fea3b939f7
Matthew M. Boedicker <matthewm@boedicker.org>
2015-08-25 21:34:33 -0700

    Output deleted AMIs and snapshots.

--- aws/ami_cleanup.sh
+++ aws/ami_cleanup.sh
@@ -11,6 +11,8 @@ aws \
   '.Images | sort_by(.CreationDate) | reverse | .[3:-1][] | .ImageId+" "+.BlockDeviceMappings[0].Ebs.SnapshotId' \
   | \
   while read AMI_ID SNAPSHOT_ID; do
+    echo "deregister AMI $AMI_ID"
     aws ec2 deregister-image --image-id $AMI_ID
+    echo "delete snapshot $SNAPSHOT_ID"
     aws ec2 delete-snapshot --snapshot-id $SNAPSHOT_ID
   done
