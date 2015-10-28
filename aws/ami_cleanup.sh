822b2e2c3f08c5222389eb8f2ca15658036501ec
Matthew M. Boedicker <matthewm@boedicker.org>
2015-10-28 00:36:59 -0700

    Fix off by one error in AMI cleanup script

--- aws/ami_cleanup.sh
+++ aws/ami_cleanup.sh
@@ -8,7 +8,7 @@ aws \
   | \
   jq \
   --raw-output \
-  '.Images | sort_by(.CreationDate) | reverse | .[3:-1][] | .ImageId+" "+.BlockDeviceMappings[0].Ebs.SnapshotId' \
+  '.Images | sort_by(.CreationDate) | reverse | .[3:][] | .ImageId+" "+.BlockDeviceMappings[0].Ebs.SnapshotId' \
   | \
   while read AMI_ID SNAPSHOT_ID; do
     echo "deregister AMI $AMI_ID"
