b2228ba3fb7da0d613a4a20b5f00a49d73409efb
Matthew M. Boedicker <matthewm@boedicker.org>
2016-04-07 21:26:08 -0700

    Create functions for opening and closing firewall hole
    This makes it easier to clean up if the ssh command fails.

--- aws/punch_hole.sh
+++ aws/punch_hole.sh
@@ -39,52 +39,64 @@ echo "nat ip address        : $NAT_IP"
 echo "nat security group id : $NAT_GROUP_ID"
 echo "web security group id : $WEB_GROUP_ID"
 
-aws \
-  ec2 \
-  authorize-security-group-ingress \
-  --group-id $WEB_GROUP_ID \
-  --protocol tcp \
-  --port 22 \
-  --source-group $NAT_GROUP_ID
+open_hole() {
+  echo "🔓  opening inbound tcp port 22 from $NAT_GROUP_ID to $WEB_GROUP_ID"
+  aws \
+    ec2 \
+    authorize-security-group-ingress \
+    --group-id $WEB_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --source-group $NAT_GROUP_ID
 
-aws \
-  ec2 \
-  authorize-security-group-egress \
-  --group-id $NAT_GROUP_ID \
-  --protocol tcp \
-  --port 22 \
-  --source-group $WEB_GROUP_ID
+  echo "🔓  opening outbound tcp port 22 from $NAT_GROUP_ID to $WEB_GROUP_ID"
+  aws \
+    ec2 \
+    authorize-security-group-egress \
+    --group-id $NAT_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --source-group $WEB_GROUP_ID
 
-aws \
-  ec2 \
-  authorize-security-group-ingress \
-  --group-id $NAT_GROUP_ID \
-  --protocol tcp \
-  --port 22 \
-  --cidr $MY_IP/32
+  echo "🔓  opening inbound tcp port 22 from $MY_IP/32 to $NAT_GROUP_ID"
+  aws \
+    ec2 \
+    authorize-security-group-ingress \
+    --group-id $NAT_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --cidr $MY_IP/32
+}
 
+close_hole() {
+  echo "🔒  closing inbound tcp port 22 from $MY_IP/32 to $NAT_GROUP_ID"
+  aws \
+    ec2 \
+    revoke-security-group-ingress \
+    --group-id $NAT_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --cidr $MY_IP/32
+
+  echo "🔒  closing outbound tcp port 22 from $NAT_GROUP_ID to $WEB_GROUP_ID"
+  aws \
+    ec2 \
+    revoke-security-group-egress \
+    --group-id $NAT_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --source-group $WEB_GROUP_ID
+
+  echo "🔒  closing inbound tcp port 22 from $NAT_GROUP_ID to $WEB_GROUP_ID"
+  aws \
+    ec2 \
+    revoke-security-group-ingress \
+    --group-id $WEB_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --source-group $NAT_GROUP_ID
+}
+
+open_hole
 ssh -A ec2-user@$NAT_IP
-
-aws \
-  ec2 \
-  revoke-security-group-ingress \
-  --group-id $NAT_GROUP_ID \
-  --protocol tcp \
-  --port 22 \
-  --cidr $MY_IP/32
-
-aws \
-  ec2 \
-  revoke-security-group-egress \
-  --group-id $NAT_GROUP_ID \
-  --protocol tcp \
-  --port 22 \
-  --source-group $WEB_GROUP_ID
-
-aws \
-  ec2 \
-  revoke-security-group-ingress \
-  --group-id $WEB_GROUP_ID \
-  --protocol tcp \
-  --port 22 \
-  --source-group $NAT_GROUP_ID
+close_hole
