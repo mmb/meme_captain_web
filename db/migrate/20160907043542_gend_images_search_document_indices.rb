1806cac6638f3a6614c22b0cad9c3717a51a498f
Matthew M. Boedicker <matthewm@boedicker.org>
2016-09-07 00:57:17 -0700

    Some values of gend_images.search_document were too big to index
    On PostgreSQL.

--- db/migrate/20160907043542_gend_images_search_document_indices.rb
+++ db/migrate/20160907043542_gend_images_search_document_indices.rb
@@ -1,7 +1,5 @@
 class GendImagesSearchDocumentIndices < ActiveRecord::Migration
   def change
-    add_index :gend_images, :search_document, length: 64
-
     if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
       execute "create index on gend_images using gin(to_tsvector('english', search_document));"
     end
