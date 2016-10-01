1c9b6d6e61e47b476f050335dbe86242bc1c408a
Matthew M. Boedicker <matthewm@boedicker.org>
2016-09-30 22:26:46 -0700

    Use RAILS_SERVE_STATIC_FILES environment variable
    Closer to the Rails default config.

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -4,7 +4,12 @@ describe 'rack middleware' do
   it 'assembles production middleware' do
     # this can catch errors in production middleware configuration
     expect(
-      system('rake middleware RAILS_ENV=production > /dev/null 2>&1')
+      system(
+        'rake middleware ' \
+        'RAILS_ENV=production ' \
+        'RAILS_SERVE_STATIC_FILES=true ' \
+        '> /dev/null 2>&1'
+      )
     ).to eq(true)
   end
 end
