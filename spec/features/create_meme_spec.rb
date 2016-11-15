bcf106892c65922d62668f2cfe7ba7f1cc28cbfe
Matthew M. Boedicker <matthewm@boedicker.org>
2016-11-14 21:10:46 -0800

    Add meme creation to quick add url feature spec

--- spec/features/create_meme_spec.rb
+++ spec/features/create_meme_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 require 'support/features'
 
-RSpec.feature 'quick add url', type: :feature, js: true do
-  scenario 'quick add url' do
+RSpec.feature 'create meme', type: :feature, js: true do
+  scenario 'create meme' do
     create_user
 
     fill_in('quick-add-url', with:
@@ -13,5 +13,16 @@ RSpec.feature 'quick add url', type: :feature, js: true do
 
     expect(page).to have_text('URL successfully submitted')
     expect(page).to have_current_path(%r{/gend_images/new\?src=.+}, wait: 10)
+
+    click_button('Add another caption')
+
+    fill_in('Caption 1', with: 'caption 1')
+    fill_in('Caption 3', with: 'caption 2')
+    fill_in('Caption 3', with: 'caption 3')
+
+    click_button('Create')
+
+    expect(page).to have_text('Submitted request')
+    expect(page).to have_current_path(%r{/gend_image_pages/.+}, wait: 10)
   end
 end
