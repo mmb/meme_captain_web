ce3bcb53508981f2b3c03555db9eef7c44f22a28
Matthew M. Boedicker <matthewm@boedicker.org>
2016-11-15 20:47:26 -0800

    Write puma stdout and stderr to log files

--- docker/puma_config.rb
+++ docker/puma_config.rb
@@ -6,3 +6,7 @@ pidfile '/run/puma.pid'
 
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
+
+stdout_redirect '/app/log/puma.out.log', '/app/log/puma.err.log', true
+
+quiet
