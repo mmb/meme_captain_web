18cf8d2ce3a4a423fa1a99ed21113a25316ff296
Matthew M. Boedicker <matthewm@boedicker.org>
2016-12-05 21:59:08 -0800

    Add if exists to src_sets_to_tsvector_idx creation

--- db/migrate/20161206051008_src_sets_search_document_indices.rb
+++ db/migrate/20161206051008_src_sets_search_document_indices.rb
@@ -1,7 +1,7 @@
 class SrcSetsSearchDocumentIndices < ActiveRecord::Migration[5.0]
   def change
     if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-      execute 'drop index src_sets_to_tsvector_idx'
+      execute 'drop index if exists src_sets_to_tsvector_idx'
       execute "create index on src_sets using gin(to_tsvector('english', search_document));"
     end
   end
