bc9af7400925a3c52053d6fca25eb6eba66cd7ac
Matthew M. Boedicker <matthewm@boedicker.org>
2017-03-01 20:40:51 -0800

    use src image captions when determining default captions
    If the src image has no captions, fall back to the default defaults.

--- app/classes/meme_captain_web/caption_builder.rb
+++ app/classes/meme_captain_web/caption_builder.rb
@@ -2,11 +2,30 @@ module MemeCaptainWeb
   # Build the default captions for a gend image.
   class CaptionBuilder
     def build(gend_image)
-      gend_image.captions.build(top_coords)
-      gend_image.captions.build(bottom_coords)
+      if gend_image.src_image.captions.empty?
+        return build_default_captions(gend_image)
+      end
+      build_src_image_default_captions(gend_image)
     end
 
     private
+
+    def build_src_image_default_captions(gend_image)
+      gend_image.src_image.captions.position_order.each do |c|
+        gend_image.captions.build(
+          top_left_x_pct: c.top_left_x_pct,
+          top_left_y_pct: c.top_left_y_pct,
+          width_pct: c.width_pct,
+          height_pct: c.height_pct,
+          text: c.text
+        )
+      end
+    end
+
+    def build_default_captions(gend_image)
+      gend_image.captions.build(top_coords)
+      gend_image.captions.build(bottom_coords)
+    end
 
     def top_coords
       {
--- spec/classes/meme_captain_web/caption_builder_spec.rb
+++ spec/classes/meme_captain_web/caption_builder_spec.rb
@@ -2,28 +2,60 @@ require 'rails_helper'
 
 describe 'MemeCaptainWeb::CaptionBuilder' do
   describe '#build' do
-    it 'builds the captions' do
-      caption_builder = MemeCaptainWeb::CaptionBuilder.new
-      gend_image = instance_double(GendImage)
-      captions = instance_double(
-        Caption::ActiveRecord_Associations_CollectionProxy
-      )
-      expect(gend_image).to receive(:captions).twice.and_return(captions)
+    let(:caption_builder) { MemeCaptainWeb::CaptionBuilder.new }
+    let(:gend_image) { FactoryGirl.create(:gend_image) }
 
-      expect(captions).to receive(:build).with(
-        top_left_x_pct: 0.05,
-        top_left_y_pct: 0,
-        width_pct: 0.9,
-        height_pct: 0.25
-      )
-      expect(captions).to receive(:build).with(
-        top_left_x_pct: 0.05,
-        top_left_y_pct: 0.75,
-        width_pct: 0.9,
-        height_pct: 0.25
-      )
+    context 'when the src image has default captions' do
+      before do
+        gend_image.src_image.captions << FactoryGirl.create(:caption)
+        gend_image.src_image.captions << FactoryGirl.create(:caption)
+      end
 
-      caption_builder.build(gend_image)
+      it "uses the src image's default captions" do
+        caption_builder.build(gend_image)
+
+        expect(gend_image.captions.size).to eq(2)
+
+        caption1 = gend_image.captions[0]
+        expect(caption1.top_left_x_pct).to eq(1.5)
+        expect(caption1.top_left_y_pct).to eq(1.5)
+        expect(caption1.width_pct).to eq(1.5)
+        expect(caption1.height_pct).to eq(1.5)
+        expect(caption1.text).to eq('MyString')
+        expect(caption1.font).to be_nil
+
+        caption2 = gend_image.captions[1]
+        expect(caption2.top_left_x_pct).to eq(1.5)
+        expect(caption2.top_left_y_pct).to eq(1.5)
+        expect(caption2.width_pct).to eq(1.5)
+        expect(caption2.height_pct).to eq(1.5)
+        expect(caption2.text).to eq('MyString')
+        expect(caption2.font).to be_nil
+      end
+    end
+
+    context 'when the src image does not have default captions' do
+      it 'uses the default captions' do
+        caption_builder.build(gend_image)
+
+        expect(gend_image.captions.size).to eq(2)
+
+        caption1 = gend_image.captions[0]
+        expect(caption1.top_left_x_pct).to eq(0.05)
+        expect(caption1.top_left_y_pct).to eq(0)
+        expect(caption1.width_pct).to eq(0.9)
+        expect(caption1.height_pct).to eq(0.25)
+        expect(caption1.text).to be_nil
+        expect(caption1.font).to be_nil
+
+        caption2 = gend_image.captions[1]
+        expect(caption2.top_left_x_pct).to eq(0.05)
+        expect(caption2.top_left_y_pct).to eq(0.75)
+        expect(caption2.width_pct).to eq(0.9)
+        expect(caption2.height_pct).to eq(0.25)
+        expect(caption2.text).to be_nil
+        expect(caption2.font).to be_nil
+      end
     end
   end
 end
